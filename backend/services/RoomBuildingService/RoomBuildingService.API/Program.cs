102f54548c0b4b393e46d5ef47ccf849c67afaa3
Hugo <khachong2102005@gmail.com>
2026-05-27 16:10:21 +0700

    update

--- backend/services/RoomBuildingService/RoomBuildingService.API/Program.cs
+++ backend/services/RoomBuildingService/RoomBuildingService.API/Program.cs
@@ -13,7 +13,16 @@ builder.Services.AddSwaggerGen();
 
 // Configure EF Core and SQL Server repository implementations
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 10,
+                maxRetryDelay: TimeSpan.FromSeconds(5),
+                errorNumbersToAdd: null
+            );
+        })
            .ConfigureWarnings(warnings =>
                warnings.Ignore(RelationalEventId.PendingModelChangesWarning)));
 
